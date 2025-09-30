7ca6b240d6b1bd239b38e6f429be4a9bc830e4e5
Abubeker-dev <yetenamereja1848@gmail.com>
2025-09-30 10:58:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const ADMIN = {
               "chapter-7":[{ title: "Amhara G5 Maths Ch1 Vid1", url: "https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&modestbranding=1" },
              { title: "Amhara G5 Maths Ch1 Vid2", url: "https://www.youtube.com/embed/3JZ_D3ELwOQ?rel=0&modestbranding=1" },  ]
             },
-            "english": { "chapter-1": [] }, "gs": { "chapter-1": [] }, "PVA": { "chapter-1": [] }, "social Study": { "chapter-1": [] }
+            "english": { "chapter-1": [] }, "gs": { "chapter-1": [] }, "ict": { "chapter-1": [] }, "citizenship": { "chapter-1": [] }
        },
        "grade-8": { // Fill with Amhara G8 data
             "maths": { "chapter-1": [] }, "english": { "chapter-1": [] }, "gs": { "chapter-1": [] }, "ict": { "chapter-1": [] }, "citizenship": { "chapter-1": [] }
@@ -629,6 +629,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 }); // End DOMContentLoaded
+
 
 
 
