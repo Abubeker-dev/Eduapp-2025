92ac18a06850db25b13120476f1c9a8f3ae2a54c
Abubeker-dev <yetenamereja1848@gmail.com>
2025-10-04 09:46:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -184,6 +184,7 @@ const ADMIN = {
               "chapter-7":[{ title: "Amhara G5 Maths Ch1 Vid1", url: "https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&modestbranding=1" },
              { title: "Amhara G5 Maths Ch1 Vid2", url: "https://www.youtube.com/embed/3JZ_D3ELwOQ?rel=0&modestbranding=1" },  ]
             },
+               
                       "maths": { "chapter-1": [] }, "english": { "chapter-1": [] },    
                           "gs": { "chapter-1": [] }, "ict": { "chapter-1": [] }, "citizenship": { "chapter-1": [] }
        },
@@ -737,6 +738,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 }); // End DOMContentLoaded
+
 
 
 
